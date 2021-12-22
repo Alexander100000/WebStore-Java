4e2a3d1212c78429c68b91f20dc13db294f86aa0
xrezat <558081xr>
2021-12-22 21:03:34 +0800

    1.第一次提交代码

---
+++ src/test/java/com/ali/alibaba_demo/AlibabaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ali.alibaba_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlibabaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
